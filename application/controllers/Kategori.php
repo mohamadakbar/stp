cba9ef967bec86a01eaef3494bc8807f914662f5
akbarmohamad1102 <mohamadakbar1102@gmail.com>
2019-04-22 12:06:26 +0700

    add kategori controller

---
+++ application/controllers/Kategori.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Kategori extends CI_Controller {
+
+  public function index()
+  { 
+    $this->load->view('v_kategori');
+  }
+
+}
+
+/* End of file Controllername.php */
